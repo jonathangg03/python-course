5477c6fb37e46672730475524be80b8bd272b620
Jonathan García <jona03g97@gmail.com>
2023-03-11 15:33:15 -0600

    Firsts steps and format

---
+++ intro.py
@@ -0,0 +1,6 @@
+"""Introducción a Python"""
+# The 1st line is necesary when we are about to create a program, it's a kind of title
+# It also can show a warning it is not an empty line at the end of the file
+
+print("Hola Mundo!")
+print(True * 4)
