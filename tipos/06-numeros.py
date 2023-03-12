601e038b5eefe321ee64136d45c7f83be603db6a
Jonathan García <jona03g97@gmail.com>
2023-03-12 12:52:29 -0600

    Numbers

---
+++ tipos/06-numeros.py
@@ -0,0 +1,18 @@
+numero = 2
+decimal = 1.2
+imaginary = 2 + 2j  # j to create an imaginary number use j, j is square root of -1
+
+#numero = numero + 2
+numero += 5
+numero *= 5
+numero -= 5
+numero /= 5
+
+
+print(1 + 3)
+print(1 - 3)
+print(1 * 3)
+print(1 / 3)  # with decimals
+print(1 // 3)  # without decimals
+print(8 % 3)  # module
+print(2 ** 3)  # number 2 elevated to 3
