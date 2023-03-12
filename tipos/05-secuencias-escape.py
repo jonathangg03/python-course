7862ddf8584c2dc5613f77676cd10387dcb01ec9
Jonathan García <jona03g97@gmail.com>
2023-03-12 12:21:31 -0600

    Strings secuences

---
+++ tipos/05-secuencias-escape.py
@@ -0,0 +1,5 @@
+# a kind of special characters in case of we want to include them in a string
+# \" : If we want to use "" inside of a string
+# \' : If we want to use ' inside of a string
+# \\ : If we want to use \ inside of a string
+# \n : A jump line.
