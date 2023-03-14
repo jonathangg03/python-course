33c29cd02d8eb63ad26c47a0af7e9c474b63e301
Jonathan García <jona03g97@gmail.com>
2023-03-13 22:00:28 -0600

    Math method

---
+++ tipos/07-numeros-funciones.py
@@ -0,0 +1,18 @@
+import math  # import a module
+
+print(round(1.3))
+print(round(1.5))
+print(abs(-45))  # Absolute value
+# These 3 functions no need math
+
+# Python doesn't have a lot of methos to work with numbers
+# But, it has a module to do that in a better way
+
+
+print(math.ceil(1.1))  # return the closest upper number
+print(math.floor(1.9999))  # return the closest lowwer number
+print(math.isnan(45))  # return if a value is a number. Only receive real numbers
+print(math.pow(10, 3))  # return the power of 2 numbers
+print(math.sqrt(9))  # return the square root of a number
+
+# For more math methods visit: https://docs.python.org/3/library/math.html
