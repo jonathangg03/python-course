feb60ed90e15059957ffcd3d4703608712901fca
Jonathan García <jona03g97@gmail.com>
2023-03-13 22:21:33 -0600

    type conversion

---
+++ tipos/08-conversion-tipos.py
@@ -0,0 +1,15 @@
+# x = input("")
+
+# int()
+# str()
+# float()
+# bool()
+
+# Falsy: void string, 0 and None
+# Truthy: the rest
+
+print(bool(""))
+print(bool("0"))
+print(bool(None))
+print(bool(" "))
+print(bool(0))
