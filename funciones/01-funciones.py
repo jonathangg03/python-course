8b2e610b7d87cd3ce08b3ad69af9fe1d4197575c
Jonathan García <jona03g97@gmail.com>
2023-03-18 13:02:22 -0600

    Arguments and parametters

--- funciones/01-funciones.py
+++ funciones/01-funciones.py
@@ -1,6 +1,11 @@
-def hola():
+def hola(nombre, apellido):  # Al defined parametters are mandatory
     print("Hola mundo")
-    print("Ultimate Python")
+    print(f"Bienvenido {nombre} {apellido}")
 
 
-hola()
+hola("Jonathan", "García")
+hola("Chanchito", "feliz")
+
+# difference beetween paremetter and argument:
+# what we use whem we define a function is a parametter
+# when we call a funcition, the value we use inside of parentesis is an argument
