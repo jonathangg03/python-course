4883d054bfa385628b997e6eb1e4cc6171d2678a
Jonathan García <jona03g97@gmail.com>
2023-03-18 12:04:41 -0600

    If

---
+++ control-flujo/02-if.py
@@ -0,0 +1,10 @@
+edad = 67
+
+if edad > 54:
+    print("Puedes entrar a ver la pelicula con descuento")
+elif edad > 17:
+    print("Puedes entrar a ver la pelicula")
+else:
+    print("No puedes ingresar a ver la pelicula")
+
+# We can use 2 spaces for identation, for convention wwe use 4
