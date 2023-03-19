d17098c37d7c3031f15d1eb94dc3508f954c0301
Jonathan García <jona03g97@gmail.com>
2023-03-19 12:57:27 -0600

    Lists

---
+++ tipos-avanzados/01-listas.py
@@ -0,0 +1,10 @@
+numeros = [1, 2, 3]
+letras = ['a', 'b', 'c']
+palabras = ['chancito', 'feliz']
+booleans = [True, False, True, True]
+matriz = [[0, 1], [1, 0]]
+ceros = [0, 1, 2] * 10  # repeat 0,1,2 10 times
+alfanumerico = numeros + letras  # join two arrays
+rango = list(range(1, 11))  # a list from 1 to 11
+chars = list("Hola mundo")  # a list with each value of that string
+print(alfanumerico)
