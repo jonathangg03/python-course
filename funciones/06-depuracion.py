caa866308a6bbd63acbdd8ed1a7fdfa9d11e07ba
Jonathan García <jona03g97@gmail.com>
2023-03-19 11:59:07 -0600

    Debugging

---
+++ funciones/06-depuracion.py
@@ -0,0 +1,10 @@
+def largo(texto):
+    resultado = 0
+    for _ in texto:
+        resultado += 1
+        # return resultado / a return here will fnish the function
+    return resultado
+
+
+print("Chanchito")
+largo("Hola mundo")
