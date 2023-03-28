e069807d6a80fa03543ac13e9f6a8e76b521927d
Jonathan García <jona03g97@gmail.com>
2023-03-27 22:35:03 -0600

    ITering lists

---
+++ tipos-avanzados/04-iterar-listas.py
@@ -0,0 +1,6 @@
+mascotas = ["pelusa", "pulga", "felipe", "chancho"]
+
+# This is how we get the index in an array
+for indice, mascota in enumerate(mascotas):
+    # With enumerate we create a tupla
+    print(indice, mascota)
